21e9672a90dba6c82c656238d535af33358c6883
Cory Castillero <corykelc07@gmail.com>
2026-04-27 17:32:27 -0400

    SSL config to prisma adpater

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,13 @@
 import { PrismaClient } from '../generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
